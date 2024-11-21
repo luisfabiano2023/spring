0301c8121b7b676533bcd2bf2dfe323e88cd3268
luisfabiano2023 <luisfabiano2022.1@gmail.com>
2024-11-20 23:35:26 -0300

    iniciando parte do projetin

---
+++ crud-fstk/crud_fullstack/src/test/java/com/example/crud_fullstack/crud_fullstack/CrudFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_fullstack.crud_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
